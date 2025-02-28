ad0908d424d5731113cbba057fd8dc92bcf2ce5e
Thanarak-q <Thanarak_ka@cmu.ac.th>
2025-02-28 10:09:25 +0700

    add setting tax

--- flask/app/models/store.py
+++ flask/app/models/store.py
@@ -7,16 +7,18 @@ class Store(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     vat = db.Column(db.Float)
-    service_charge = db.Column(db.Float)  # เพิ่ม attribute นี้
+    service_charge = db.Column(db.Float) 
     Max_Orders_per_Round = db.Column(db.Integer)
     Max_Food_Quantity_per_Order = db.Column(db.Integer)
+    Tax = db.Column(db.String(100))
 
-    def __init__(self, name, vat, service_charge, Max_Orders_per_Round, Max_Food_Quantity_per_Order):
+    def __init__(self, name, vat, service_charge, Max_Orders_per_Round, Max_Food_Quantity_per_Order, Tax):
         self.name = name
         self.vat = vat
         self.service_charge = service_charge
         self.Max_Orders_per_Round = Max_Orders_per_Round
         self.Max_Food_Quantity_per_Order = Max_Food_Quantity_per_Order
+        self.Tax = Tax
 
     def update_name(self, name):
         self.name = name
@@ -41,5 +43,8 @@ class Store(db.Model, SerializerMixin):
     def update_service_charge(self, service_charge):
         self.service_charge = service_charge
         db.session.commit()
-
+        
+    def update_tax(self, Tax):
+        self.Tax = Tax
+        db.session.commit()
     
